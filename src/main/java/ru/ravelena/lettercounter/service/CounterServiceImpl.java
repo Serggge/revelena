7aaf5980457464817f9b6db2f286c597b16ff98d
Serggge69 <serggge69@yandex.ru>
2023-09-14 13:23:37 +0300

    add sorting

--- src/main/java/ru/ravelena/lettercounter/service/CounterServiceImpl.java
+++ src/main/java/ru/ravelena/lettercounter/service/CounterServiceImpl.java
@@ -1,8 +1,11 @@
 package ru.ravelena.lettercounter.service;
 
 import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class CounterServiceImpl implements CounterService {
@@ -13,6 +16,12 @@ public class CounterServiceImpl implements CounterService {
             int count = result.getOrDefault((char) ch, 0);
             result.put((char) ch, ++count);
         });
-        return result;
+        return result.entrySet()
+                .stream()
+                .sorted(Map.Entry.<Character, Integer>comparingByValue().reversed())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
     }
 }
